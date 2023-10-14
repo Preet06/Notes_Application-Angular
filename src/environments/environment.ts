956ecba5353636962bd1dbfdcc7f7149f178ec0d
Preet06 <preetnandeshwar8@gmail.com>
2023-10-14 15:55:52 +0530

    Added frontened

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'fire-crud-3d6bd',
+    appId: '1:752210516576:web:b6ece41c43db338c574105',
+    storageBucket: 'fire-crud-3d6bd.appspot.com',
+    apiKey: 'AIzaSyCC12Berx1QGvWSMD2yf940DO4mjpeFJGU',
+    authDomain: 'fire-crud-3d6bd.firebaseapp.com',
+    messagingSenderId: '752210516576',
+  },
+};
